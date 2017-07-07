382cfbd8e07b9e5595a7484f76d680b2326a4c5b
nibhoria <nibhorian96@gmail.com>
2017-07-07 21:25:32 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'library_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR + '/books/templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
